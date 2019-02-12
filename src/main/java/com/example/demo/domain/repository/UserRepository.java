dd578bd3d6b031686b67404d3943fcfe4e0908e6
pengwenwu <pww932589183@163.com>
2019-02-12 20:42:22 +0800

    fix

--- src/main/java/com/example/demo/domain/repository/UserRepository.java
+++ src/main/java/com/example/demo/domain/repository/UserRepository.java
@@ -4,9 +4,6 @@ import com.example.demo.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    List<User> findAll();
 }
