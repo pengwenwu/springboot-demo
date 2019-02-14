2276e53101668d473bee3fbfd4a78063f0eb2d3b
pengwenwu <pengwenwu@corp-ci.com>
2019-02-14 16:26:29 +0800

    add:增加统一异常处理

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,12 +1,13 @@
 package com.example.demo.controller;
 
+import com.example.demo.exception.MyException;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
     @RequestMapping("/hello")
-    public String index() {
-        return "hello world";
+    public String index() throws MyException {
+        throw new MyException("访问hello发生错误");
     }
 }
