c2c8bfe25a2730de071750d195a4821262323b80
pengwenwu <pengwenwu@corp-ci.com>
2019-02-14 15:35:11 +0800

    add:增加swagger

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,6 +2,10 @@ package com.example.demo.controller;
 
 import com.example.demo.domain.entity.User;
 import com.example.demo.domain.repository.UserRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,15 +14,22 @@ import java.util.*;
 
 @RestController
 @RequestMapping(value = "/users")
+@Api(value = "系统用户")
 public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    @ApiOperation(value = "用户列表", notes = "获取用户列表")
     @RequestMapping(value="/", method=RequestMethod.GET)
     public List<User> getUserList() {
         return userRepository.findAll();
     }
 
+    @ApiOperation(value = "创建用户", notes = "根据参数创建user对象")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "name", value = "姓名", required = true, dataType = "String"),
+            @ApiImplicitParam(name = "age", value = "年龄", required = true, dataType = "Int")
+    })
     @RequestMapping(value="/", method=RequestMethod.POST)
     public String postUser(
             @RequestParam String name,
